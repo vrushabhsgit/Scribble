639dd8a459557379db703cf215577064477a0880
Dev-Vrushabh <dwrushabh@gmail.com>
2026-05-01 15:47:24 +0530

    set it up the ws server

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,1 +1,21 @@
-console.log("WS backend is running");
+import { WebSocketServer } from "ws";
+
+const PORT = 8080;
+const wss = new WebSocketServer({ port: PORT });
+wss.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.send("Hello from WebSocket server");
+
+  socket.on("message", (message) => {
+    console.log("Recived :", message.toString());
+
+    //echo back
+    socket.send(`Server received: ${message}`);
+  });
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+console.log(`WebSocket server running on ws://localhost:${PORT}`);
